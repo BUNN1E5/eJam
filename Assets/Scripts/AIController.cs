8f1c2cb46b6528094610e57e6bd3e83c2dcef644
Carter <andrewscart@gmail.com>
2020-06-27 16:55:27 -0700

    AI waypoints

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -9,22 +9,64 @@ public class AIController : MonoBehaviour
     public float destinationThreshold;
     public LayerMask navLayer;
     public float walkLength;
+    [Range(0,1)]
+    public float suspicion;
+    public Transform[] idleWaypoints;
+    int waypointIndex;
+    public float waypointTaskTime;
+    public float visionRadius;
+    [System.Serializable]
+    public enum state{
+        Busy,
+        Idle,
+        Suspicous,
+
+    }
+    public state myState;
+    public LayerMask suspiciousObjects;
     // Start is called before the first frame update
     void Start()
     {
         agent=GetComponent<NavMeshAgent>();
-        agent.SetDestination(GetRandomPosition());
+        waypointIndex=idleWaypoints.Length;
+        StartCoroutine(FindNewDestination(waypointTaskTime));
+        suspicion=0;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Vector3.Distance(transform.position,agent.destination)<destinationThreshold){
-            DestinationReached();
+    {   
+        
+        
+        if(myState==state.Idle){
+            if(Vector3.Distance(transform.position,agent.destination)<destinationThreshold){
+                StartCoroutine(FindNewDestination(waypointTaskTime));
+            }
+            CheckForSuspiciousObjects();
         }
     }
-    void DestinationReached(){
-        agent.SetDestination(GetRandomPosition());
+    IEnumerator FindNewDestination(float waitTime){
+        myState=state.Busy;
+        agent.isStopped=true;
+        yield return new WaitForSeconds(waitTime);
+        myState=state.Idle;     
+        agent.isStopped=false;
+        if(waypointIndex>=idleWaypoints.Length-1){
+            waypointIndex=0;
+        }
+        else{
+            waypointIndex++;
+        }
+        Vector3 nextWaypoint=idleWaypoints[waypointIndex].position;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition (nextWaypoint, out navHit, 1,navLayer); 
+        agent.SetDestination(navHit.position); 
+    }
+    void CheckForSuspiciousObjects(){
+        RaycastHit[] hits=Physics.SphereCastAll(transform.position,visionRadius,Vector3.zero,0,suspiciousObjects);
+        foreach(RaycastHit hit in hits){
+            suspicion=1;
+        }
     }
     Vector3 GetRandomPosition(){
                                   
