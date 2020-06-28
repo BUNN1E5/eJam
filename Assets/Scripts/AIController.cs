842c90c473df909219347dc51f167e80f74ee6ff
Carter <andrewscart@gmail.com>
2020-06-27 20:52:00 -0700

    decoratiob ai detection

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -9,18 +9,23 @@ public class AIController : MonoBehaviour
     public float destinationThreshold;
     public LayerMask navLayer;
     public float walkLength;
-    [Range(0,1)]
-    public float suspicion;
+    public int suspicionProgress;
     public Transform[] idleWaypoints;
     int waypointIndex;
     public float waypointTaskTime;
+    public float suspicionSpeed;
     public float visionRadius;
+    [Range(0,1)]
+    public float fovRange;
+
+    GameObject subjectOfSuspician;
     [System.Serializable]
     public enum state{
         Busy,
-        Idle,
+        Walking,
         Suspicous,
 
+        Investigating,
     }
     public state myState;
     public LayerMask suspiciousObjects;
@@ -30,26 +35,36 @@ public class AIController : MonoBehaviour
         agent=GetComponent<NavMeshAgent>();
         waypointIndex=idleWaypoints.Length;
         StartCoroutine(FindNewDestination(waypointTaskTime));
-        suspicion=0;
+        suspicionProgress=0;
     }
 
     // Update is called once per frame
     void Update()
     {   
-        
-        
-        if(myState==state.Idle){
+        if(myState==state.Suspicous){
+            if(Vector3.Distance(transform.position,agent.destination)<destinationThreshold){               
+                StartCoroutine(GrowSuspician());
+            }
+        } 
+        if(myState==state.Walking){
             if(Vector3.Distance(transform.position,agent.destination)<destinationThreshold){
                 StartCoroutine(FindNewDestination(waypointTaskTime));
             }
             CheckForSuspiciousObjects();
         }
     }
+    IEnumerator GrowSuspician(){
+        myState=state.Investigating;
+        yield return new WaitForSeconds(suspicionSpeed);
+        suspicionProgress++;
+        Destroy(subjectOfSuspician);
+        myState=state.Walking;
+    }
     IEnumerator FindNewDestination(float waitTime){
         myState=state.Busy;
         agent.isStopped=true;
         yield return new WaitForSeconds(waitTime);
-        myState=state.Idle;     
+        myState=state.Walking;     
         agent.isStopped=false;
         if(waypointIndex>=idleWaypoints.Length-1){
             waypointIndex=0;
@@ -63,14 +78,26 @@ public class AIController : MonoBehaviour
         agent.SetDestination(navHit.position); 
     }
     void CheckForSuspiciousObjects(){
-        RaycastHit[] hits=Physics.SphereCastAll(transform.position,visionRadius,Vector3.zero,0,suspiciousObjects);
-        foreach(RaycastHit hit in hits){
-            suspicion=1;
+        Collider[] hits=Physics.OverlapSphere(transform.position,visionRadius,suspiciousObjects);
+        foreach(Collider hit in hits){
+            Vector3 toTarget = (hit.transform.position - transform.position).normalized;
+             
+            if (Vector3.Dot(toTarget, transform.forward) > fovRange) {
+                RaycastHit los;
+                Debug.DrawLine(hit.transform.position,transform.position);
+                if(Physics.Raycast(transform.position,hit.transform.position-transform.position,out los,visionRadius)){
+                    if(los.collider.CompareTag("Sus")){
+                        myState=state.Suspicous;
+                        NavMeshHit navHit;
+                        NavMesh.SamplePosition(los.point, out navHit, 1,navLayer); 
+                        agent.SetDestination(navHit.position); 
+                        subjectOfSuspician=los.collider.gameObject;
+                    }
+                }
+            }
         }
     }
     Vector3 GetRandomPosition(){
-                                  
-        
         Vector3 randomDirection=Random.onUnitSphere;
         randomDirection.y=0;
         randomDirection.Normalize();
